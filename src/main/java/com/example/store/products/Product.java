de3782683045a85f1d940e88e05a570df09cfd32
katrinalin <zhaoyanlin2016@gmail.com>
2018-09-19 15:15:51 +0800

    feature: create Product and Get mapping in controller

---
+++ src/main/java/com/example/store/products/Product.java
@@ -0,0 +1,51 @@
+package com.example.store.products;
+
+import java.math.BigDecimal;
+
+public class Product {
+    private String name;
+    private BigDecimal price;
+    private String unit;
+    private Integer totalAmount;
+    private String imgUrl;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public Integer getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(Integer totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+}
